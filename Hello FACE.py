6f61f1b88b9fc84ac2261435522515622ca38c9a
thiagofelicioalves <thiagofelicio@hotmail.com>
2018-09-19 02:44:57 -0300

    Adicionado Classe

--- Hello FACE.py
+++ Hello FACE.py
@@ -3,8 +3,8 @@ from Facebot_Extrabom import Facebook
 # Precisa que ChromeDriver esteja no path
 
 facebot = Facebook.Facebook()
-facebot.setLogin(login='thiagofelicio@hotmail.com',
-                 senha='Thiago&991010')  # LOGIN E SENHA A QUAL O BOT VAI LOGAR E POSTAR
+facebot.setLogin(login='[YOUR EMAIL FACEBOOK]',
+                 senha='[YOUR PASSWORD]')  # LOGIN E SENHA A QUAL O BOT VAI LOGAR E POSTAR
 groupID = ['1889352911359628']  # ID DO GRUPO QUE DESEJA FAZER O POST
 
 facebot.setGrouopIds(groupID)
