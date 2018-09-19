d9a0a799a3be484e137cfcf495ada5eed37e06b0
thidevs <thiagofelicio@hotmail.com>
2018-09-19 16:48:09 -0300

    Send Image on Facebook Post

--- Facebot_Extrabom/Facebook.py
+++ Facebot_Extrabom/Facebook.py
@@ -7,6 +7,7 @@ class Facebook(object):
         self.driver = ''
         self.Text = ''
         self.groupID = ''
+        self.driver = None
 
     def setWebDriver(self, driver):
         self.driver = driver
@@ -24,7 +25,33 @@ class Facebook(object):
     def setText(self, text):
         self.Text = text
 
-    def start(self):
+
+    def start(self,repeat=1,posts=[]):
+
+        if repeat != len(posts) and repeat != 1:
+            raise ValueError("Repetition value is not equal to number of posts")
+
+        def PostAgain(driver):
+            element = driver.find_element_by_name('xhpc_message_text')
+            element.click()
+            time.sleep(2)
+
+            element = driver.find_element_by_xpath(
+                '/html/body/div[1]/div[3]/div[1]/div/div[2]/div[2]/div[2]/div[2]/div[2]/div[2]/div/div/div[2]/div[1]/div/div/div/div[2]/div/div[1]/div/div[1]/div[2]/div/div/div/div/div/div/div[2]/div')
+
+            element.send_keys(self.Text)
+            time.sleep(3)
+
+            PostImage(driver)
+            time.sleep(10)
+            driver.find_element_by_css_selector('._1mf7').click()
+
+        def PostImage(driver):
+
+            element = driver.find_element_by_name('composer_photo[]')
+            element.send_keys("C:\\Users\\thiago.alves\\Desktop\\Hydrangeas.jpg")
+
+
 
         import getpass
         chrome_options = webdriver.ChromeOptions()
@@ -33,35 +60,29 @@ class Facebook(object):
         chrome_options.add_argument("user-data-dir=C:/Users/"+str(getpass.getuser())+"/AppData/Local/Google/Chrome/User Data/Default")
 
         path = "C:\\Program Files\\ChromeDriver\\chromedriver.exe"
-
-
         driver = webdriver.Chrome(executable_path=path, chrome_options=chrome_options)
-        #driver.get(url)
-        try:
-            for id in self.groupID:
+        self.driver = driver
+        for id in self.groupID:
+            url = 'https://www.facebook.com/groups/' + id
+            driver.get(url)
+            try:
+                driver.find_element_by_xpath('//*[@id="email"]').send_keys(self.Login)
+                driver.find_element_by_xpath('//*[@id="pass"]').send_keys(self.Senha)
+                driver.find_element_by_xpath('//*[@id="loginbutton"]').click()
+            except Exception:
+                pass
+            time.sleep(5)
+            self.driver.execute_script("scroll(0, +250);")
+        if repeat == 1:
+            if len(posts) != 1:
+                repeat = len(posts)
 
-                url = 'https://www.facebook.com/groups/' + id
-                driver.get(url)
+        for i in range(repeat):
+            if posts != '':
+                self.Text = posts[i]
+            PostAgain(driver)
+            time.sleep(2)
 
-                try:
-                    driver.find_element_by_xpath('//*[@id="email"]').send_keys(self.Login)
-                    driver.find_element_by_xpath('//*[@id="pass"]').send_keys(self.Senha)
-                    driver.find_element_by_xpath('//*[@id="loginbutton"]').click()
-                except Exception:
-                    pass
 
-                time.sleep(5)
-                element = driver.find_element_by_name('xhpc_message_text')
-                element.click()
-                time.sleep(2)
-
-                element = driver.find_element_by_xpath(
-                    '/html/body/div[1]/div[3]/div[1]/div/div[2]/div[2]/div[2]/div[2]/div[2]/div[2]/div/div/div[2]/div[1]/div/div/div/div[2]/div/div[1]/div/div[1]/div[2]/div/div/div/div/div/div/div[2]/div')
-                element.send_keys(self.Text)
-
-                driver.execute_script("scroll(0, +250);")
-                driver.find_element_by_css_selector('._1mf7').click()
-                time.sleep(5)
-
-        finally:
-            driver.close()
+    def stop(self):
+        self.driver.close()
--- Hello FACE.py
+++ Hello FACE.py
@@ -5,10 +5,13 @@ from Facebot_Extrabom import Facebook
 facebot = Facebook.Facebook()
 facebot.setLogin(login='[YOUR EMAIL FACEBOOK]',
                  senha='[YOUR PASSWORD]')  # LOGIN E SENHA A QUAL O BOT VAI LOGAR E POSTAR
-groupID = ['1889352911359628']  # ID DO GRUPO QUE DESEJA FAZER O POST
 
+groupID = ['1889352911359628']  # ID DO GRUPO QUE DESEJA FAZER O POST
 facebot.setGrouopIds(groupID)
-print(facebot.getLogin())
-for i in range(5):
-    facebot.setText(str(i) + " mensagem")
-    facebot.start()
+
+Posts = ['Post com Imagem']
+
+
+facebot.start(posts=Posts)
+
+facebot.stop()
